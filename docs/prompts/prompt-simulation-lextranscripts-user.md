66cc540fd154c27be140168ff7132621dce002b3
Vincent Koc <vincentkoc@ieee.org>
2023-11-18 22:05:29 +1100

    Update simulation

--- docs/prompts/prompt-simulation-lextranscripts-user.md
+++ docs/prompts/prompt-simulation-lextranscripts-user.md
@@ -1,14 +1,26 @@
-You task is to simulate a conversation between a person in an affected area for a bushfire evacuation in Gippsland VIC.
+Your task is to simulate a detailed conversation between an emergency services agent and a person in Gippsland VIC, affected by a bushfire evacuation. The conversation should cover various aspects of the evacuation process, including initial contact, confirmation of the person's awareness of the bushfire, evacuation instructions, safety precautions, and closure of the conversation.
 
-This should be the full conversation, including the initial greeting and the final goodbye.
+Ensure the conversation includes:
+1. An initial greeting and verification of the person's location.
+2. Questions about the person's awareness of the bushfire and whether they have received evacuation orders.
+3. Detailed instructions on evacuation procedures, including suggested routes, essential items to carry, and safety measures.
+4. Information on assistance available, such as evacuation centers or emergency services contact.
+5. Check if the person requires any special assistance, like medical support or transportation.
+6. Instructions on how to stay updated with the latest information about the bushfire.
+7. A final message ensuring the person understands all instructions, with an offer for further help if needed, followed by a goodbye.
+8. unique_contact_id should just be a random id for this transcript
 
 Provide the response in the following format for Lex:
-```
+
 {
     "Participants": [
         {
-            "ParticipantId": "string",
-            "ParticipantRole": "AGENT | CUSTOMER"
+            "ParticipantId": "EmergencyAgent",
+            "ParticipantRole": "AGENT"
+        },
+        {
+            "ParticipantId": "Resident",
+            "ParticipantRole": "CUSTOMER"
         }
     ],
     "Version": "1.1.0",
@@ -16,17 +28,22 @@ Provide the response in the following format for Lex:
         "RedactionTypes": [
             "PII"
         ],
-        "Output": "Raw | Redacted"
+        "Output": "Raw"
     },
     "CustomerMetadata": {
-        "ContactId": "string"
+        "ContactId": "unique_contact_id"
     },
     "Transcript": [
         {
-            "ParticipantId": "string",
-            "Id": "string",
-            "Content": "string"
+            "ParticipantId": "EmergencyAgent",
+            "Id": "message_id",
+            "Content": "message_content"
+        },
+        {
+            "ParticipantId": "Resident",
+            "Id": "message_id",
+            "Content": "message_content"
         }
+        // More conversation messages following the same pattern
     ]
 }
-```
--- platform/simulateconversations/simulate.py
+++ platform/simulateconversations/simulate.py
@@ -4,6 +4,7 @@ import os
 from openai import OpenAI
 from dotenv import load_dotenv
 from tqdm import tqdm
+import concurrent.futures
 
 # Set up logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
@@ -12,6 +13,7 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(
 load_dotenv()
 api_key = os.getenv("OPENAI_API_KEY")
 number_of_simulations = 100
+concurrent_simulations = 10  # Number of simulations to run concurrently
 
 # Specify the folder to save the JSON files
 output_folder = "../../data/simulations"
@@ -25,6 +27,39 @@ user_prompt_file = "../../docs/prompts/prompt-simulation-lextranscripts-user.md"
 with open(user_prompt_file, 'r') as file:
     user_prompt = file.read()
 
+# Function to handle each simulation
+def handle_simulation(i):
+    try:
+        # Generate the conversation using OpenAI's chat completion API
+        response = client.chat.completions.create(
+            model="gpt-3.5-turbo-1106",
+            messages=[
+                {"role": "system", "content": system_prompt},
+                {"role": "user", "content": user_prompt}
+            ],
+            max_tokens=1024,
+            n=1,
+            stop=None,
+            temperature=0.7,
+            timeout=30,
+        )
+
+        # Extract the generated message from the API response
+        message = response.choices[0].message.content
+
+        # Parse the message content as JSON
+        message_json = json.loads(message)
+
+        # Save the conversation as a JSON file
+        filename = f"{output_folder}/simulation_{i+1}.json"
+        with open(filename, 'w') as file:
+            json.dump(message_json, file, indent=4)  # Pretty print
+
+        logging.info(f"Simulation {i+1} completed and saved as {filename}")
+
+    except Exception as e:
+        logging.error(f"Error in simulation {i+1}: {e}")
+
 # Init the OpenAI client
 try:
     client = OpenAI(api_key=api_key)
@@ -32,44 +67,6 @@ except Exception as e:
     logging.error(f"Failed to initialize OpenAI client: {e}")
     exit(1)
 
-# Run the simulation n times
-for i in tqdm(range(number_of_simulations), desc="Simulating conversations"):
-    # Generate the conversation using OpenAI's chat completion API
-    response = client.chat.completions.create(
-        model="gpt-3.5-turbo-1106",
-        messages=[
-            {
-                "role": "system",
-                "content": system_prompt
-            },
-            {
-                "role": "user",
-                "content": user_prompt
-            }
-        ],
-        #response_format={ "type": "json_object" },
-        max_tokens=1024,
-        n=1,
-        stop=None,
-        temperature=0.7,
-        timeout=30,
-    )
-
-    # Extract the generated message from the API response
-    message = response.choices[0].message.content
-
-    # Parse the message content as JSON
-    try:
-        message_json = json.loads(message)
-    except json.JSONDecodeError:
-        logging.error("Failed to decode JSON from the message content")
-        continue  # Skip this iteration if the message content is not valid JSON
-
-    # Save the conversation as a JSON file
-    filename = f"{output_folder}/simulation_{i+1}.json"
-    with open(filename, 'w') as file:
-        json.dump(message_json, file, indent=4)  # Pretty print
-
-
-    logging.info(f"Simulation {i+1} completed and saved as {filename}")
-    # exit(1)
+# Run the simulations concurrently
+with concurrent.futures.ThreadPoolExecutor(max_workers=concurrent_simulations) as executor:
+    list(tqdm(executor.map(handle_simulation, range(number_of_simulations)), total=number_of_simulations, desc="Simulating conversations"))
