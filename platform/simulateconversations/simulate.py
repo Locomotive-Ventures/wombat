10ae6a3c3ae65b20347fbd9f462f23de1735c146
Vincent Koc <vincentkoc@ieee.org>
2023-11-18 21:04:00 +1100

    Update simulate.py

--- platform/simulateconversations/simulate.py
+++ platform/simulateconversations/simulate.py
@@ -1,21 +1,29 @@
 import json
+import logging
 import openai
+import os
+from dotenv import load_dotenv
+from tqdm import tqdm
 
-# Set up your OpenAI API key
-openai.api_key = 'YOUR_API_KEY'
+# Set up logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-# Define the prompt for the conversation simulation
-prompt = """
-You are simulating a conversation using OpenAI.
+# Load API key from .env file
+load_dotenv()
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
-Conversation Simulation Prompt: Write your prompt here.
-"""
+# Define the path to the prompt file
+prompt_file = "../../docs/prompts/prompt-simulation-lextranscripts.md"
+
+# Read the prompt from the file
+with open(prompt_file, 'r') as file:
+    prompt = file.read()
 
 # Run the simulation 100 times
-for i in range(100):
+for i in tqdm(range(100), desc="Simulating conversations"):
     # Generate the conversation using OpenAI's chat completion API
     response = openai.Completion.create(
-        engine='text-davinci-003',
+        model="gpt-4-1106-preview",
         prompt=prompt,
         max_tokens=100,
         n=1,
@@ -31,4 +39,4 @@ for i in range(100):
     with open(filename, 'w') as file:
         json.dump({'prompt': prompt, 'message': message}, file)
 
-    print(f"Simulation {i+1} completed and saved as {filename}")
+    logging.info(f"Simulation {i+1} completed and saved as {filename}")
