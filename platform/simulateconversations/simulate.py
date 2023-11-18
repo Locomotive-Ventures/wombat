3c95a57e4ece54e89fcef096b5665d01a57963ce
Vincent Koc <vincentkoc@ieee.org>
2023-11-18 21:35:27 +1100

    Update simulate.py

--- platform/simulateconversations/simulate.py
+++ platform/simulateconversations/simulate.py
@@ -47,7 +47,7 @@ for i in tqdm(range(number_of_simulations), desc="Simulating conversations"):
                 "content": user_prompt
             }
         ],
-        response_format={ "type": "json_object" },
+        #response_format={ "type": "json_object" },
         max_tokens=1024,
         n=1,
         stop=None,
@@ -58,10 +58,18 @@ for i in tqdm(range(number_of_simulations), desc="Simulating conversations"):
     # Extract the generated message from the API response
     message = response.choices[0].message.content
 
+    # Parse the message content as JSON
+    try:
+        message_json = json.loads(message)
+    except json.JSONDecodeError:
+        logging.error("Failed to decode JSON from the message content")
+        continue  # Skip this iteration if the message content is not valid JSON
+
     # Save the conversation as a JSON file
     filename = f"{output_folder}/simulation_{i+1}.json"
     with open(filename, 'w') as file:
-        json.dump({'prompt': prompt, 'message': message}, file, indent=4)  # Pretty print
+        json.dump(message_json, file, indent=4)  # Pretty print
+
 
     logging.info(f"Simulation {i+1} completed and saved as {filename}")
     exit(1)
