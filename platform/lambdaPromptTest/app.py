c83b2733d9243efb3e2bf86a339a2d6cce956bc3
Vincent Koc <vincentkoc@ieee.org>
2023-11-20 10:25:09 +1100

    Update app.py

--- platform/lambdaPromptTest/app.py
+++ platform/lambdaPromptTest/app.py
@@ -1,38 +1,56 @@
 import json
 import os
-from openai import OpenAI
+import logging
+from openai import OpenAI, OpenAIError
+
+# Configure logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 def lambda_handler(event, context):
-    # Initialize the OpenAI client
-    client = get_openai_client()
+    try:
+        # Initialize the OpenAI client
+        client = get_openai_client()
 
-    # Generate the response using OpenAI's chat completion API
-    response_content = get_openai_response(event, client)
+        # Generate the response using OpenAI's chat completion API
+        response_content = get_openai_response(event, client)
 
-    # Return the response
-    return {
-        'statusCode': 200,
-        'body': json.dumps({'response': response_content})
-    }
+        # Return the successful response
+        return {
+            'statusCode': 200,
+            'body': json.dumps({'response': response_content})
+        }
+
+    except Exception as e:
+        logging.error(f"Error in lambda_handler: {str(e)}")
+        return {
+            'statusCode': 500,
+            'body': json.dumps({'error': 'Internal server error'})
+        }
 
 def get_openai_client():
-    # Get API key
-    api_key = os.getenv("OPENAI_API_KEY")
-
-    # Initialize the OpenAI client
     try:
+        # Get API key
+        api_key = os.getenv("OPENAI_API_KEY")
+        if not api_key:
+            raise ValueError("Missing OPENAI_API_KEY environment variable")
+
+        # Initialize the OpenAI client
         client = OpenAI(api_key=api_key)
         return client
+
+    except OpenAIError as openai_error:
+        logging.error(f"OpenAI API error: {openai_error}")
+        raise
     except Exception as e:
-        print("Failed to load OpenAI client:", e)
-        raise e
+        logging.error(f"Error initializing OpenAI client: {e}")
+        raise
 
 def get_openai_response(event, client):
-    # Extract input from the event object
-    user_input = event['queryStringParameters']['message']
-
-    # Generate the response using OpenAI's chat completion API
     try:
+        # Extract input from the event object
+        user_input = event['queryStringParameters']['message']
+
+        # Generate the response using OpenAI's chat completion API
         response = client.chat.completions.create(
             model="gpt-3.5-turbo-1106",
             messages=[
@@ -48,15 +66,22 @@ def get_openai_response(event, client):
         generated_message = response.choices[0].message.content
         return generated_message
 
+    except OpenAIError as openai_error:
+        logging.error(f"OpenAI API error: {openai_error}")
+        raise ValueError("Error communicating with OpenAI API")
     except Exception as e:
-        return f"Error: {str(e)}"
+        logging.error(f"Error generating response: {e}")
+        raise ValueError("Error processing the input")
 
 if __name__ == "__main__":
-    client = get_openai_client()
-    event = {
-        'queryStringParameters': {
-            'message': 'Hello, how are you?'
+    try:
+        client = get_openai_client()
+        event = {
+            'queryStringParameters': {
+                'message': 'Hello, how are you?'
+            }
         }
-    }
-    response = lambda_handler(event, None)
-    print(response)
+        response = lambda_handler(event, None)
+        print(response)
+    except Exception as e:
+        logging.error(f"Runtime error: {e}")
