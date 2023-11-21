c82c880f297281ba38c5c6a78d2bb30ff2b10aeb
larriquin <33453129+larriquin@users.noreply.github.com>
2023-11-21 23:00:19 +1100

    chore: Reformat file using terraform fmt command

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 terraform {
-    required_providers {
+  required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.26"
@@ -11,7 +11,7 @@ terraform {
   backend "s3" {
     bucket         = "tf-s3-backend"
     key            = "/terraform.tfstate"
-    region         = "ap-southeast-2"  # Change this to your desired AWS region
+    region         = "ap-southeast-2" # Change this to your desired AWS region
     encrypt        = true
     dynamodb_table = "tf-statelock"
   }
@@ -28,9 +28,9 @@ resource "aws_s3_bucket" "tf-s3-backend" {
 }
 
 resource "aws_dynamodb_table" "tf-statelock" {
-  name           = "tf-statelock"
-  billing_mode   = "PAY_PER_REQUEST"
-  hash_key       = "LockID"
+  name         = "tf-statelock"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
   attribute {
     name = "LockID"
     type = "S"
