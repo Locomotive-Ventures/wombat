ac4a99769c66b3053125bdd886e1c974c531bb14
Vincent Koc <vincentkoc@ieee.org>
2023-11-25 00:45:54 +1100

    Update twillio.tf

--- infrastructure/terraform/operations/twillio.tf
+++ infrastructure/terraform/operations/twillio.tf
@@ -1,4 +1,5 @@
 resource "twilio_api_accounts_incoming_phone_numbers" "phone_number" {
+  account_sid   = "SK22afeb94a7a8693ce7713cf482aa2f75"
   area_code     = "61"
   friendly_name = "terraform phone number"
   sms_url       = "https://demo.twilio.com/welcome/sms/reply"
