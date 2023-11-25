82176059615b8de4f966e0743e92fe3b11ccbf1f
Vincent Koc <vincentkoc@ieee.org>
2023-11-25 14:38:24 +1100

    Update versions.tf

--- infrastructure/.terraformer/versions.tf
+++ infrastructure/.terraformer/versions.tf
@@ -2,8 +2,8 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.50.0"
+      version = "~> 5.26"
     }
   }
-  required_version = "~> 1.6.4"
+  required_version = "1.3.7"
 }
