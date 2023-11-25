fd39372aebf350b748ae627389229a590febd843
Vincent Koc <vincentkoc@ieee.org>
2023-11-25 17:09:11 +1100

    Update test_twillio.py

--- platform/lambdaTwillioConvo/test_twillio.py
+++ platform/lambdaTwillioConvo/test_twillio.py
@@ -1,6 +1,11 @@
-from twilio.rest import Client
 import os
+import logging
+from twilio.rest import Client
+from twilio.base.exceptions import TwilioRestException
 from dotenv import load_dotenv
+
+# Initialize logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 # Load environment variables
 load_dotenv("../../.env")
@@ -12,26 +17,40 @@ twilio_twiml_url = os.getenv("TWILIO_TWIML_URL")
 twilio_from_number = os.getenv("TWILIO_FROM_NUMBER")
 recipient_number = '+61404638881'  # Replace with the recipient's number
 
-print(f"SID length: {len(twilio_account_sid)}")
-print(f"Token length: {len(twilio_auth_token)}")
-print("-")
-print(f"twilio_account_sid: {twilio_account_sid}")
-print(f"twilio_auth_token: {twilio_auth_token}")
-print(f"twilio_twiml_url: {twilio_twiml_url}")
-print(f"twilio_from_number: {twilio_from_number}")
-print(f"recipient_number: {recipient_number}")
+# Logging environment variable details (for debugging, remove in production)
+logging.debug(f"SID length: {len(twilio_account_sid)}")
+logging.debug(f"Token length: {len(twilio_auth_token)}")
+logging.debug(f"Twilio Account SID: {twilio_account_sid}")
+logging.debug(f"Twilio Auth Token: {twilio_auth_token}")
+logging.debug(f"Twilio TWIML URL: {twilio_twiml_url}")
+logging.debug(f"Twilio From Number: {twilio_from_number}")
+logging.debug(f"Recipient Number: {recipient_number}")
 
-# Initialize Twilio client
-twilio_client = Client(
-    twilio_account_sid,
-    twilio_auth_token
-)
+try:
+    # Initialize Twilio client
+    twilio_client = Client(twilio_account_sid, twilio_auth_token)
 
-call = twilio_client.calls.create(
-    to=recipient_number,
-    from_=twilio_from_number,
-    url=twilio_twiml_url
-)
+    # Create and log call
+    call = twilio_client.calls.create(
+        to=recipient_number,
+        from_=twilio_from_number,
+        url=twilio_twiml_url
+    )
 
-print(f"Call initiated from: {twilio_from_number} to: {recipient_number}")
-print(call)
+    logging.info(f"Call initiated from: {twilio_from_number} to: {recipient_number}")
+    logging.info(f"Call SID: {call.sid}")
+    logging.info(f"Call Status: {call.status}")
+
+except TwilioRestException as e:
+    logging.error(f"Twilio API error: {e}")
+except Exception as e:
+    logging.error(f"General error: {e}")
+
+# Polling for call completion (optional, based on your requirement)
+# import time
+# while True:
+#     updated_call = twilio_client.calls(call.sid).fetch()
+#     logging.info(f"Updated Call Status: {updated_call.status}")
+#     if updated_call.status == "completed":
+#         break
+#     time.sleep(5)  # Poll every 5 seconds
