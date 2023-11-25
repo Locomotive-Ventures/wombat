1e5a0471e845a2b3888484776328f6150fc17f4b
Vincent Koc <vincentkoc@ieee.org>
2023-11-25 17:12:04 +1100

    Update test_twillio.py

--- platform/lambdaTwillioConvo/test_twillio.py
+++ platform/lambdaTwillioConvo/test_twillio.py
@@ -1,5 +1,6 @@
 import os
 import logging
+import time
 from twilio.rest import Client
 from twilio.base.exceptions import TwilioRestException
 from dotenv import load_dotenv
@@ -17,15 +18,6 @@ twilio_twiml_url = os.getenv("TWILIO_TWIML_URL")
 twilio_from_number = os.getenv("TWILIO_FROM_NUMBER")
 recipient_number = '+61404638881'  # Replace with the recipient's number
 
-# Logging environment variable details (for debugging, remove in production)
-logging.debug(f"SID length: {len(twilio_account_sid)}")
-logging.debug(f"Token length: {len(twilio_auth_token)}")
-logging.debug(f"Twilio Account SID: {twilio_account_sid}")
-logging.debug(f"Twilio Auth Token: {twilio_auth_token}")
-logging.debug(f"Twilio TWIML URL: {twilio_twiml_url}")
-logging.debug(f"Twilio From Number: {twilio_from_number}")
-logging.debug(f"Recipient Number: {recipient_number}")
-
 try:
     # Initialize Twilio client
     twilio_client = Client(twilio_account_sid, twilio_auth_token)
@@ -39,18 +31,24 @@ try:
 
     logging.info(f"Call initiated from: {twilio_from_number} to: {recipient_number}")
     logging.info(f"Call SID: {call.sid}")
-    logging.info(f"Call Status: {call.status}")
+
+    # Polling for call completion
+    while True:
+        print(".", end="", flush=True)  # Print a dot without newline
+        updated_call = twilio_client.calls(call.sid).fetch()
+
+        if updated_call.status != call.status:
+            call = updated_call
+            logging.info(f"Call Status Updated: {call.status}")
+
+        if call.status == "completed" or call.status == "failed":
+            break
+
+        time.sleep(5)  # Poll every 5 seconds
+
+    logging.info(f"Final Call Status: {call.status}")
 
 except TwilioRestException as e:
     logging.error(f"Twilio API error: {e}")
 except Exception as e:
     logging.error(f"General error: {e}")
-
-# Polling for call completion (optional, based on your requirement)
-# import time
-# while True:
-#     updated_call = twilio_client.calls(call.sid).fetch()
-#     logging.info(f"Updated Call Status: {updated_call.status}")
-#     if updated_call.status == "completed":
-#         break
-#     time.sleep(5)  # Poll every 5 seconds
