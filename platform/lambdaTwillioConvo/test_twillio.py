e02c3adf140d2c67c19aeb630a3bac5b6ec52a73
Vincent Koc <vincentkoc@ieee.org>
2023-11-25 17:50:51 +1100

    functioning trigger calls

--- platform/lambdaTwillioConvo/test_twillio.py
+++ platform/lambdaTwillioConvo/test_twillio.py
@@ -14,7 +14,7 @@ load_dotenv("../../.env")
 # Config and Credentials
 twilio_account_sid = os.getenv("TWILIO_ACCOUNT_SID", "").strip()
 twilio_auth_token = os.getenv("TWILIO_AUTH_TOKEN", "").strip()
-twilio_twiml_url = os.getenv("TWILIO_TWIML_URL")
+twilio_flow_sid = os.getenv("TWILIO_FLOW_SID", "").strip()
 twilio_from_number = os.getenv("TWILIO_FROM_NUMBER")
 recipient_number = '+61404638881'  # Replace with the recipient's number
 
@@ -22,31 +22,36 @@ try:
     # Initialize Twilio client
     twilio_client = Client(twilio_account_sid, twilio_auth_token)
 
+    # Execute Studio Flow
+    execution = twilio_client.studio \
+        .flows(twilio_flow_sid) \
+        .executions \
+        .create(to=recipient_number, from_=twilio_from_number)
     # Create and log call
-    call = twilio_client.calls.create(
-        to=recipient_number,
-        from_=twilio_from_number,
-        url=twilio_twiml_url
-    )
+    # call = twilio_client.calls.create(
+    #     to=recipient_number,
+    #     from_=twilio_from_number,
+    #     url=twilio_twiml_url
+    # )
 
-    logging.info(f"Call initiated from: {twilio_from_number} to: {recipient_number}")
-    logging.info(f"Call SID: {call.sid}")
+    logging.info(f"Call initiated via flow {twilio_flow_sid} from: {twilio_from_number} to: {recipient_number}")
+    logging.info(f"Call execution SID: {execution.sid}")
 
     # Polling for call completion
-    while True:
-        print(".", end="", flush=True)  # Print a dot without newline
-        updated_call = twilio_client.calls(call.sid).fetch()
+    # while True:
+    #     print(".", end="", flush=True)  # Print a dot without newline
+    #     updated_call = twilio_client.calls(call.sid).fetch()
 
-        if updated_call.status != call.status:
-            call = updated_call
-            logging.info(f"Call Status Updated: {call.status}")
+    #     if updated_call.status != call.status:
+    #         call = updated_call
+    #         logging.info(f"Call Status Updated: {call.status}")
 
-        if call.status == "completed" or call.status == "failed":
-            break
+    #     if call.status == "completed" or call.status == "failed":
+    #         break
 
-        time.sleep(5)  # Poll every 5 seconds
+    #     time.sleep(5)  # Poll every 5 seconds
 
-    logging.info(f"Final Call Status: {call.status}")
+    # logging.info(f"Final Call Status: {call.status}")
 
 except TwilioRestException as e:
     logging.error(f"Twilio API error: {e}")
