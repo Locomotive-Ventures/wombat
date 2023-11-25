5fc40eb0eeb36211fc319d3525248bcc816fa5e8
Vincent Koc <vincentkoc@ieee.org>
2023-11-25 14:47:36 +1100

    test change

--- infrastructure/terraform/storage/s3.tf
+++ infrastructure/terraform/storage/s3.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "s3_bucket_static_website" {
-  bucket = "wombat-warden-s3-static-website"
+  bucket = "wombat-warden-s3-static-website22"
 
   lifecycle {
     prevent_destroy = true
